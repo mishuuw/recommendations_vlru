d5c60474a0cd4ebdebe48a34f8453c92888795f0
миша <osipenko.ma@dvfu.ru>
2024-10-23 12:27:04 +1000

    master.py fix (un-commented lines)

--- master.py
+++ master.py
@@ -139,5 +139,5 @@ def getrecommendedevents():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-'''if __name__ == '__main__':
-    app.run(debug=True)'''
+if __name__ == '__main__':
+    app.run(debug=True)
